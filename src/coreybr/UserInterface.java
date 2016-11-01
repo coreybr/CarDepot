55ded822e4a60d8f98d2f6d1218b5820a185f959
Corey Bradley <coreytbradley@gmail.com>
2016-11-01 19:45:19 -0400

    Make header locked at top with scrolling content

--- src/coreybr/UserInterface.java
+++ src/coreybr/UserInterface.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -31,11 +32,6 @@ public class UserInterface implements Runnable {
 
 		createComponents(frame.getContentPane());
 
-		// Put container into scrollable area
-		JScrollPane myJScrollPane = new JScrollPane(frame.getContentPane(), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		frame.setContentPane(myJScrollPane);
-
 		frame.pack();
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // maximized
 		frame.setVisible(true);
@@ -43,87 +39,87 @@ public class UserInterface implements Runnable {
 
 	// Define GUI content
 	private void createComponents(Container container) {
-		// Setup layout
-		GridLayout layout = new GridLayout(cars.size() + 2, 13);
-		container.setLayout(layout);
-
-		/*
-		 * Setup GUI elements
-		 */
-
+		// Setup container layout
+		container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
+		
 		// Header
+		Container headerContainer = new Container();
+		GridLayout headerLayout = new GridLayout(1, cars.size());
+		headerContainer.setLayout(headerLayout);
+		
 		makeLabel = new JLabel("Make");
 		makeLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
-		container.add(makeLabel);
+		headerContainer.add(makeLabel);
 		modelLabel = new JLabel("Model");
-		container.add(modelLabel);
+		headerContainer.add(modelLabel);
 		typeLabel = new JLabel("Type");
-		container.add(typeLabel);
+		headerContainer.add(typeLabel);
 		minPriceLabel = new JLabel("Min price");
-		container.add(minPriceLabel);
+		headerContainer.add(minPriceLabel);
 		maxPriceLabel = new JLabel("Max price");
-		container.add(maxPriceLabel);
+		headerContainer.add(maxPriceLabel);
 		mpgCityLabel = new JLabel("MPG City");
-		container.add(mpgCityLabel);
+		headerContainer.add(mpgCityLabel);
 		mpgHwyLabel = new JLabel("MPG Hwy");
-		container.add(mpgHwyLabel);
+		headerContainer.add(mpgHwyLabel);
 		airbagsLabel = new JLabel("Airbags");
-		container.add(airbagsLabel);
+		headerContainer.add(airbagsLabel);
 		drivetrainLabel = new JLabel("Drivetrain");
-		container.add(drivetrainLabel);
+		headerContainer.add(drivetrainLabel);
 		cylindersLabel = new JLabel("Cylinders");
-		container.add(cylindersLabel);
+		headerContainer.add(cylindersLabel);
 		horsepowerLabel = new JLabel("Horsepower");
-		container.add(horsepowerLabel);
+		headerContainer.add(horsepowerLabel);
 		manTransAvailLabel = new JLabel("Man Trans Avail");
-		container.add(manTransAvailLabel);
+		headerContainer.add(manTransAvailLabel);
 		originLabel = new JLabel("Origin");
-		container.add(originLabel);
+		headerContainer.add(originLabel);
+		container.add(headerContainer);
 
-		// Blank row
-		for (int i = 0; i < layout.getColumns(); i++) {
-			makeLabel = new JLabel();
-			container.add(makeLabel);
-		}
+		// Setup bodyContainer layout
+		Container bodyContainer = new Container();
+		GridLayout bodyLayout = new GridLayout(cars.size(), 13);
+		bodyContainer.setLayout(bodyLayout);
+		
+		// Put container into scrollable area
+		JScrollPane myJScrollPane = new JScrollPane(bodyContainer, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
 		// Show cars
 		for (Car car : cars) {
 			makeLabel = new JLabel(car.getManufacturer());
 			makeLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
-			container.add(makeLabel);
+			bodyContainer.add(makeLabel);
 			modelLabel = new JLabel(car.getModel());
-			container.add(modelLabel);
+			bodyContainer.add(modelLabel);
 			typeLabel = new JLabel(car.getType());
-			container.add(typeLabel);
+			bodyContainer.add(typeLabel);
 			minPriceLabel = new JLabel("$" + Double.toString(car.getMinPrice()) + "0");
-			container.add(minPriceLabel);
+			bodyContainer.add(minPriceLabel);
 			maxPriceLabel = new JLabel("$" + Double.toString(car.getMaxPrice()) + "0");
-			container.add(maxPriceLabel);
+			bodyContainer.add(maxPriceLabel);
 			mpgCityLabel = new JLabel(Integer.toString(car.getMpgCity()));
-			container.add(mpgCityLabel);
+			bodyContainer.add(mpgCityLabel);
 			mpgHwyLabel = new JLabel(Integer.toString(car.getMpgHighway()));
-			container.add(mpgHwyLabel);
+			bodyContainer.add(mpgHwyLabel);
 			airbagsLabel = new JLabel(car.getAirbags());
-			container.add(airbagsLabel);
+			bodyContainer.add(airbagsLabel);
 			drivetrainLabel = new JLabel(car.getDrivetrain());
-			container.add(drivetrainLabel);
+			bodyContainer.add(drivetrainLabel);
 			cylindersLabel = new JLabel(Integer.toString(car.getCylinders()));
-			container.add(cylindersLabel);
+			bodyContainer.add(cylindersLabel);
 			horsepowerLabel = new JLabel(Integer.toString(car.getHorsepower()));
-			container.add(horsepowerLabel);
+			bodyContainer.add(horsepowerLabel);
 			if (car.isManTransAvailable()) {
 				manTransAvailLabel = new JLabel("Yes");
 			} else {
 				manTransAvailLabel = new JLabel("No");
 			}
-			container.add(manTransAvailLabel);
+			bodyContainer.add(manTransAvailLabel);
 			originLabel = new JLabel(car.getOrigin());
-			container.add(originLabel);
+			bodyContainer.add(originLabel);
 		}
-
-		/*
-		 * End: Setup GUI elements
-		 */
+		container.add(myJScrollPane);
 	}
 
 }
