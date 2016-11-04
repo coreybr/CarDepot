904848edd2dbb338cd2694fdb14fd15cf85d29b0
Corey Bradley <coreytbradley@gmail.com>
2016-11-04 19:22:26 -0400

    Make columns correct data class and sortable, uneditable

--- src/coreybr/UserInterface.java
+++ src/coreybr/UserInterface.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class UserInterface implements Runnable {
 
@@ -34,10 +35,10 @@ public class UserInterface implements Runnable {
 	}
 
 	// Define GUI content
+	@SuppressWarnings("rawtypes")
 	private void createComponents(Container container) {
-		
-		
-		// Create table
+
+		// Create table column names
 		Vector<String> columnNames = new Vector<String>();
 		columnNames.addElement("Make");
 		columnNames.addElement("Model");
@@ -51,42 +52,57 @@ public class UserInterface implements Runnable {
 		columnNames.addElement("Cylinders");
 		columnNames.addElement("Horsepower");
 		columnNames.addElement("Man Trans Avail");
-		columnNames.addElement("Origin");		
+		columnNames.addElement("Origin");
 
 		// Populate table rows
-		@SuppressWarnings("rawtypes")
 		Vector<Vector> rowData = new Vector<Vector>();
-		
-		for (Car car : cars) {		
-			Vector<String> row = new Vector<String>();
-		    row.addElement(car.getManufacturer());
-		    row.addElement(car.getModel());
-		    row.addElement(car.getType());
-		    row.addElement("$" + Double.toString(car.getMinPrice()) + "0");
-		    row.addElement("$" + Double.toString(car.getMaxPrice()) + "0");
-		    row.addElement(Integer.toString(car.getMpgCity()));
-		    row.addElement(Integer.toString(car.getMpgHighway()));
-		    row.addElement(car.getAirbags());
-		    row.addElement(car.getDrivetrain());
-		    row.addElement(Integer.toString(car.getCylinders()));
-		    row.addElement(Integer.toString(car.getHorsepower()));
-		    if (car.isManTransAvailable()) {
-		    	row.addElement("Yes");
-			} else {
-				row.addElement("No");
-			}
-		    row.addElement(car.getOrigin());
-		    
-		    rowData.addElement(row);
+
+		for (Car car : cars) {
+			Vector<Comparable> row = new Vector<Comparable>();
+			row.addElement(car.getManufacturer());
+			row.addElement(car.getModel());
+			row.addElement(car.getType());
+			row.addElement(car.getMinPrice());
+			row.addElement(car.getMaxPrice());
+			row.addElement(car.getMpgCity());
+			row.addElement(car.getMpgHighway());
+			row.addElement(car.getAirbags());
+			row.addElement(car.getDrivetrain());
+			row.addElement(car.getCylinders());
+			row.addElement(car.getHorsepower());
+			row.addElement(car.isManTransAvailable());
+			row.addElement(car.getOrigin());
+			rowData.addElement(row);
 		}
-		
-		JTable table = new JTable(rowData, columnNames);
-		
-		// Put table into scrollable area
-		JScrollPane myJScrollPane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		// Create table model
+		DefaultTableModel tableModel = new DefaultTableModel(rowData, columnNames) {
+
+			@Override
+			public Class<?> getColumnClass(int col) {
+
+				Class retVal = Object.class;
+
+				if (getRowCount() > 0)
+					retVal = getValueAt(0, col).getClass();
+
+				return retVal;
+			}
+
+			@Override
+			public boolean isCellEditable(int row, int col) {
+				return false;
+			}
+		};
+
+		// Create table
+		JTable table = new JTable(tableModel);
+		table.setAutoCreateRowSorter(true);
+
+		// Put table into scroll pane
+		JScrollPane myJScrollPane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		myJScrollPane.getVerticalScrollBar().setUnitIncrement(16); // Increase scroll speed
-		
+
+		// Add scroll pane to container
 		container.add(myJScrollPane);
 	}
 
